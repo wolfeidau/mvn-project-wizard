713b867ce94296d05209fae5ef494ad85dbc2f84
Mark Wolfe <mark@wolfe.id.au>
2011-07-29 15:58:55 +1000

    Created a new rails app as a starting place.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MavenProjectWizard::Application.config.secret_token = '2fb1ea10f9a78a4684bf270a81502d73e6e7cfdf639d152493cf4a1628a8d4adea2910e92ec69f9fd2cc6c09fedc084ff11571974844f3a434ad03d72815d0c7'
