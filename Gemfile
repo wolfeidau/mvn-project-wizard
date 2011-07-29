1cc1b6cbb3c49ba1ea5c6ae1bef54f286e1a2779
Mark Wolfe <mark@wolfe.id.au>
2011-07-29 20:01:44 +1000

    Added backbone.js and a welcome view

--- Gemfile
+++ Gemfile
@@ -29,3 +29,4 @@ gem 'jquery-rails'
 
 gem 'therubyracer'
 gem 'execjs'
+gem 'backbone-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MavenProjectWizard::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
